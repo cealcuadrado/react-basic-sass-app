6c211bfaaabdfdcf14b350ad268d6217816d23e5
CeAlCuadrado <cealcuadrado@outlook.com>
2021-10-28 20:59:37 -0300

    Establecido layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.scss';
+import Layout from './layout/Layout';
 
 function App() {
   return (
     <div className="App">
       <h2>React: Basic SASS App</h2>
+      <hr />
+      <Layout/>
     </div>
   );
 }
---
+++ src/layout/Layout.js
@@ -0,0 +1,32 @@
+import './Layout.scss';
+
+function Layout() {
+    return (
+      <div className="layout">
+        <h3>Layout</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in
+          arcu nibh. Vestibulum ultrices maximus nisl, in posuere velit euismod
+          sed. Duis lobortis aliquam maximus. Morbi fermentum ut neque id
+          feugiat. Maecenas vulputate dolor ut est tincidunt mattis et eu eros.
+          Suspendisse porttitor eu purus vitae faucibus. Quisque vel maximus
+          libero, vel eleifend justo. In vel lacus vel orci pharetra vestibulum.
+          Donec vitae dictum mi. Etiam consectetur quis tortor in semper. Mauris
+          posuere tristique ligula.
+        </p>
+        <p>
+          Aliquam auctor pulvinar ultrices. Phasellus non congue mauris, eu
+          imperdiet magna. Praesent eget nisi felis. Duis tristique condimentum
+          ante, quis commodo neque ornare vel. Duis blandit elit eu massa
+          laoreet, quis mollis metus tristique. Nunc eget justo purus. Nullam
+          non bibendum tellus. Nullam auctor ligula vitae turpis accumsan
+          gravida. Phasellus viverra nisl maximus sapien feugiat, vitae molestie
+          nisi mollis. Pellentesque vitae libero sed quam elementum tempus nec
+          quis enim. Mauris eleifend sem sit amet porttitor lacinia. Aenean eu
+          ullamcorper felis.
+        </p>
+      </div>
+    );
+}
+
+export default Layout;
